7bfaac18900884a250e937afd9c0670b867cd282
Breno Tiso <brenotiso@hotmail.com>
2019-04-04 23:14:59 -0300

    Update and rename Cliente.py to cliente.py

--- cliente.py
+++ cliente.py
@@ -2,13 +2,24 @@ import socket, time, sys, urllib.request, urllib.parse, urllib.error
 
 ## ip servidor
 IP = "18.204.102.146"
-## porta
-PORTA = 7100
+## portas
+PORTA_UDP = 7100
+PORTA_TCP = 7200
 ## mensagem a ser enviada
 MENSAGEM = "Ping!" 
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.settimeout(2)
+sock_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock_tcp.settimeout(5)
+destino_tcp = (IP, PORTA_TCP)
+
+sock_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock_udp.settimeout(2)
+destino_udp = (IP, PORTA_UDP)
+
+#fazer a parte dos parametros aqui (verificar se vai querer fazer o ping)
+sock_tcp.connect(destino_tcp)
+mensagem_inicial = 'fazer ping' 
+sock_tcp.send(mensagem_inicial.encode())
 
 pacotesEnviar = 10
 rttSoma = 0
@@ -19,15 +30,16 @@ pacotesPerdidos = 0
 primeiroPacoteRecebido = False
 ultimoRtt = 0
 somaJitter = 0
-sock.connect((IP, PORTA))
-print("Estatísticas para {0}:".format(IP))
+
+print("\nTestando Ping:".format(IP))
 for i in range(pacotesEnviar):
     time.sleep(1)
     pacotesEnviados += 1
     tempoEnvio = time.time()
-    sock.send(MENSAGEM.encode())
+    sock_udp.sendto(MENSAGEM.encode(), destino_udp)
+
     try:
-        resposta, server = sock.recvfrom(1024)
+        resposta, server = sock_udp.recvfrom(1024)
         tempoChegada = time.time()
         rtt = (tempoChegada - tempoEnvio) * 1000 # armazenando o tempo em ms
         rttSoma += rtt
@@ -48,21 +60,28 @@ for i in range(pacotesEnviar):
         print('Esgotado o tempo limite.')
         pacotesPerdidos += 1
 
+
+
 ## informacoes do ping
 if(pacotesEnviar > 1):
-    print("\nTestando Ping:".format(IP))
+    print("\nEstatísticas para {0}:".format(IP))
     print("    Pacotes: Enviados = {0}, Recebidos = {1}, Perdidos = {2} ({3:.2f}% de perdas)".format(pacotesEnviados, pacotesRecebidos, pacotesPerdidos, ( (pacotesPerdidos / pacotesEnviar) * 100 ) ))
     print("    Média de Ping = {0}ms".format( round( (rttSoma / pacotesRecebidos), 2) ) )
     if(pacotesEnviar > 2):
         print("    Jitter = {0}ms".format( round( (somaJitter / (pacotesRecebidos - 1) ), 2) ) )
 
+mensagem_fim_ping = 'fim ping' 
+sock_tcp.send(mensagem_fim_ping.encode())
 
-print("\nTestando o Download: ")
-url = ""
-tempo = time.time()
-f = urllib.request.urlopen(url)
-data = f.read()
-print(len(data))
-print((len(data) / (1024*1024 / 8)) / (time.time() - tempo), "Mbps")
+mensagem_fim = 'fim' 
+sock_tcp.send(mensagem_fim.encode())
+
+#print("\nTestando o Download: ")
+#url = ""
+#tempo = time.time()
+#f = urllib.request.urlopen(url)
+#data = f.read()
+#print(len(data))
+#print((len(data) / (1024*1024 / 8)) / (time.time() - tempo), "Mbps")
 #with open("arq.pdf", "wb") as code:
 #    code.write(data)
