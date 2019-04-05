1183ebe8daa39b0e90b904b024badd0004dcbba6
Breno Tiso <brenotiso@hotmail.com>
2019-04-04 23:14:36 -0300

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -13,33 +13,34 @@ udp.bind((IP, PORTA_UDP))
 
 conn, addr = tcp.accept()
 while True:
-	data = conn.recv(1024)
-	if(data.decode() == 'fazer ping'):
-		conn_udp, addr_udp = udp.accept()
-		while True:
-			data = conn_udp.recv(1024)
-			if(data.decode() == 'fim ping'):
-				conn_udp.close()
-				break
-			else:
-				conn_udp.send(message)  # echo
-		
-	if(data.decode() == 'fazer download'):
-		#vai usar a mesma conexao tcp
-		while True:
-			##enviar arquivo
-			
-	if(data.decode() == 'fazer upload'):
-		#vai usar a mesma conexao tcp
-		while True:
-			##receber arquivo
-			
-	if(data.decode() == 'fim'):
-		conn.close()
-		break
+        data = conn.recv(1024)
+        if(data.decode() == 'fazer ping'):
+                while True:
+                        data, addr = udp.recvfrom(1024)
+                        if(data.decode() == 'fim ping'):
+                                break
+                        else:
+                                udp.sendto(data, addr)  # echo
+
+        '''
+        if(data.decode() == 'fazer download'):
+                #vai usar a mesma conexao tcp
+                while True:
+                        ##enviar arquivo
+
+        if(data.decode() == 'fazer upload'):
+                #vai usar a mesma conexao tcp
+                while True:
+                        ##receber arquivo
+        '''
+
+        if(data.decode() == 'fim'):
+                print("chegou no fim")
+                conn.close()
+                break
 
 
-'''		
+'''
 conn, addr = s.accept()
 print 'Endereco de conexao: ', addr
 data = conn.recv(1024) #Tamanho do buffer.
@@ -47,9 +48,9 @@ print ("Mensagem Recebida:", data.decode())
 message = data.upper()
 conn.send(message)
 while data != "exit":
-	data = conn.recv(1024) #Tamanho do buffer.
-	print ("Mensagem Recebida:", data.decode())
-	message = data.upper()
-	conn.send(message)  # echo
+        data = conn.recv(1024) #Tamanho do buffer.
+        print ("Mensagem Recebida:", data.decode())
+        message = data.upper()
+        conn.send(message)  # echo
 conn.close()
 '''
