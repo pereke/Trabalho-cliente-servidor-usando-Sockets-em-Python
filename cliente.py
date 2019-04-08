a58d7f3dc3eb90327e9a58f39f7f178091a87016
Breno Tiso <brenotiso@hotmail.com>
2019-04-08 14:44:29 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -7,7 +7,7 @@ import urllib.error
 import os
 
 # ip servidor
-IP = "18.204.102.146"
+IP = "ibiza.dcc.ufla.br"
 #IP = "localhost"
 # portas
 PORTA_UDP = 7100
@@ -16,7 +16,7 @@ PORTA_TCP = 7200
 MENSAGEM = "Rtt!"
 
 sock_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock_tcp.settimeout(5)
+#sock_tcp.settimeout(5)
 destino_tcp = (IP, PORTA_TCP)
 
 sock_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -67,7 +67,7 @@ def rtt():
             pacotesPerdidos += 1
 
     # informacoes do rtt
-    if (pacotesEnviar > 1):
+    if (pacotesRecebidos > 0):
         print("\nEstatísticas para {0}:".format(IP))
         print("    Pacotes: Enviados = {0}, Recebidos = {1}, Perdidos = {2} ({3:.2f}% de perdas)".format(pacotesEnviados, pacotesRecebidos, pacotesPerdidos, ( (pacotesPerdidos / pacotesEnviar) * 100 ) ))
         if(pacotesRecebidos > 0):
@@ -80,17 +80,20 @@ def rtt():
 
 
 def download():
-	global sock_tcp
-	f = open('recebidoD.bin','wb')
-	parte = sock_tcp.recv(1024)
-	i = 1
-	#while(parte):
-	for i in range(1, 30720):
-		i = i + 1
-		f.write(parte)
-		parte = sock_tcp.recv(1024)
-	f.write(parte)
-	f.close()
+    global sock_tcp
+    f = open('recebidoD.bin','wb')
+    inicio = time.time()
+    parte = sock_tcp.recv(1024)
+    i = 1
+    for i in range(1, 30720):
+        i = i + 1
+        f.write(parte)
+        parte = sock_tcp.recv(1024)
+    f.write(parte)
+    f.close()
+
+    fim = time.time()
+    print(((os.stat('recebidoD.bin').st_size) / (1024*1024 / 8)) / (fim - inicio), "Mbps")
     
 
 def upload():
@@ -127,28 +130,34 @@ while True:
     op = input("Escolha uma opcao: ")
 
     if (op == '1'):
+        print("\nTestando o RTT + Jitter:")
         sock_tcp.send('fazer rtt'.encode())
         rtt()
         sock_tcp.send('fim rtt'.encode())
         time.sleep(3)
 
     elif (op == '2'):
+        print("\nTestando o download:")
         sock_tcp.send('fazer download'.encode())
         download()
 
     elif (op == '3'):
+        print("\nTestando o upload:")
         sock_tcp.send('fazer upload'.encode())
         upload()
 
     elif (op == '4'):
         sock_tcp.send('fazer rtt'.encode())
+        print("\nTestando o RTT + Jitter:")
         rtt()
         sock_tcp.send('fim rtt'.encode())
         time.sleep(3)
         sock_tcp.send('fazer download'.encode())
+        print("\nTestando o download:")
         download()
         time.sleep(3)
         sock_tcp.send('fazer upload'.encode())
+        print("\nTestando o upload:")
         upload()
 
     else:
